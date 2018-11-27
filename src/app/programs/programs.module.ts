d7517939da0fff83551de3ce6fabbf0dfb004058
Youssef Sherif <sharief@aucegypt.edu>
2018-11-27 23:18:33 +0200

    Add program component

--- src/app/programs/programs.module.ts
+++ src/app/programs/programs.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProgramsListComponent } from './programs-list/programs-list.component';
+import { ProgramComponent } from './program/program.component';
 
 @NgModule({
-  declarations: [ProgramsListComponent],
+  declarations: [ProgramsListComponent, ProgramComponent],
   imports: [
     CommonModule
   ]
