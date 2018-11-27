9bd5ce8f2a31053de4606dff62a555d262cda64b
Youssef Sherif <sharief@aucegypt.edu>
2018-11-27 23:17:35 +0200

    Add programs list

---
+++ src/app/programs/programs-list/programs-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-programs-list',
+  templateUrl: './programs-list.component.html',
+  styleUrls: ['./programs-list.component.css']
+})
+export class ProgramsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/programs/programs.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProgramsListComponent } from './programs-list/programs-list.component';
+
+@NgModule({
+  declarations: [ProgramsListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProgramsModule { }
