4a6f7f3ef281e01c84d3f6a1aa2c14e14633b1d4
Youssef Sherif <sharief@aucegypt.edu>
2018-11-30 19:50:05 +0200

    Update activity list component

--- src/app/activities/activities-list/activities-list.component.spec.ts
+++ src/app/activities/activities-list/activities-list.component.spec.ts
@@ -6,48 +6,69 @@ import * as fromRoot from '../../reducers';
 import * as fromFeature from '../activities.reducer';
 import { SharedModule } from '../../shared/shared.module';
 import { FetchActivitiesSuccess } from '../activities.actions';
+import { FetchDataSuccess } from '../../app.actions';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ActivitiesListComponent', () => {
-  let component: ActivitiesListComponent;
-  let fixture: ComponentFixture<ActivitiesListComponent>;
-  let store: Store<fromFeature.State>;
+    let component: ActivitiesListComponent;
+    let fixture: ComponentFixture<ActivitiesListComponent>;
+    let store: Store<fromFeature.State>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        SharedModule,
-        StoreModule.forRoot({
-          ...fromRoot.reducers,
-          feature: combineReducers(fromFeature.reducer),
-        }),
-      ],
-      declarations: [ActivitiesListComponent]
-    })
-      .compileComponents();
-    store = TestBed.get(Store);
-    spyOn(store, 'dispatch').and.callThrough();
-    fixture = TestBed.createComponent(ActivitiesListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                SharedModule,
+                StoreModule.forRoot({
+                    ...fromRoot.reducers,
+                    feature: combineReducers(fromFeature.reducer),
+                }),
+            ],
+            declarations: [ActivitiesListComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute, useValue: {
+                        snapshot: {
+                            params: {
+                                programId: 1
+                            }
+                        },
+                    }
+                }
 
-  }));
+            ]
+        })
+            .compileComponents();
+        store = TestBed.get(Store);
+        spyOn(store, 'dispatch').and.callThrough();
+        fixture = TestBed.createComponent(ActivitiesListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    }));
 
-
-  it('should display a list of items after the data is loaded', () => {
-    const items = [{
-      id: 4, name: 'a',
-      startDate: '12/04/1998', endDate: '12/04/1998'}];
-    const action = new FetchActivitiesSuccess(items);
-
-    store.dispatch(action);
-
-    component.activities$.subscribe(data => {
-      expect(data.length).toBe(items.length);
+    it('should create', () => {
+        expect(component).toBeTruthy();
     });
-  });
+
+
+    it('should display a list of activities', () => {
+        const fakePrograms = Array.from({ length: 20 }, (_, i) => ({ id: i + 20, name: 'a' }));
+        const fakeActivities = Array.from({ length: 5 }, (_, i) =>
+            ({ id: i + 20, programId: 2, name: 'p', startDate: '12/4/1998', endDate: '12/4/1998' }))
+            .concat(
+                Array.from({ length: 30 }, (_, i) =>
+                    ({ id: i + 20, programId: 1, name: 'a', startDate: '12/4/1998', endDate: '12/4/1998' }))
+            );
+
+        const payload = { programs: fakePrograms, activities: fakeActivities };
+        const action = new FetchDataSuccess(payload);
+        store.dispatch(action);
+
+        component.activities$.subscribe(data => {
+            expect(data.length).toBe(10);
+        });
+    });
 
 });
