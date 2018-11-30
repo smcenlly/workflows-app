ca873da613a34f56a0f5875dc236c2976c96f53c
Youssef Sherif <sharief@aucegypt.edu>
2018-12-01 00:59:47 +0200

    Update utility

--- src/app/core/services/utility.ts
+++ src/app/core/services/utility.ts
@@ -1,15 +1,15 @@
 import { apiBaseUrl } from 'src/app/config/constants';
 import { Activity } from '../../activities/models/activity';
 
-export const addWorkflowLevel1Prop = (activity: Activity): Activity =>
-    ({ ...activity, workflowlevel1: `${apiBaseUrl}/workflowlevel1/${activity.programId}` });
+export const addWorkflowLevel1Prop = (activity: Activity, programId: number): Activity =>
+    ({ ...activity, workflowlevel1: `${apiBaseUrl}/workflowlevel1/${programId}/` });
 
 
 export const addProgramIdProp = (activity: Activity): Activity =>
     ({ ...activity, programId: parseInt(activity.workflowlevel1.split('/')[5], 10) });
 
 
-export const tenBasedOnPageNumber = (pageNumber, arr) => {
+export const tenBasedOnPageNumber = (pageNumber: number, arr: any[]) => {
     const firstIndex = (pageNumber - 1) * 10;
     return arr.slice(firstIndex, firstIndex + 10);
 };
