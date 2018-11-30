a191de0df537654e18d572a12c4ba490af09b2f0
Youssef Sherif <sharief@aucegypt.edu>
2018-11-30 22:49:12 +0200

    Update activity model

--- src/app/activities/models/activity.ts
+++ src/app/activities/models/activity.ts
@@ -1,8 +1,8 @@
 export interface Activity {
     id: number;
     name: string;
-    expected_start_date: string;
-    expected_end_date: string;
+    expected_start_date?: string;
+    expected_end_date?: string;
     workflowlevel1?: string;
     programId?: number;
 }
