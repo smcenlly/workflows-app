23b636350176f02bfd962893ff9b132e51e1a646
Youssef Sherif <sharief@aucegypt.edu>
2018-11-30 19:50:24 +0200

    Add new apis

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -6,21 +6,31 @@ import { Activity } from 'src/app/activities/models/Activity';
 import { apiBaseUrl } from 'src/app/config/constants';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
-  private readonly firstLevelUrl = `${apiBaseUrl}/workflowlevel1/`;
-  private readonly secondLevelUrl = `${apiBaseUrl}/workflowlevel2/`;
-  constructor(private http: HttpClient) { }
+    private readonly firstLevelUrl = `${apiBaseUrl}/workflowlevel1/`;
+    private readonly secondLevelUrl = `${apiBaseUrl}/workflowlevel2/`;
+    constructor(private http: HttpClient) { }
 
-  getPrograms(): Observable<Program[]> {
-    console.log('in api service');
-    return this.http.get<Program[]>(this.firstLevelUrl);
-  }
+    getPrograms(): Observable<Program[]> {
+        return this.http.get<Program[]>(this.firstLevelUrl);
+    }
 
-  getActivitiesForProgram(programId): Observable<Activity[]> {
-    console.log('in api service');
-    return this.http.get<Activity[]>(`${this.secondLevelUrl}/?workflowlevel1__id=${programId}`);
-  }
+    getActivities(): Observable<Activity[]> {
+        return this.http.get<Activity[]>(`${this.secondLevelUrl}`);
+    }
+
+    addActivity(programId: number, activity: Activity): Observable<Activity> {
+        return this.http.post<Activity>(`${this.secondLevelUrl}/${programId}`, activity);
+    }
+
+    deleteActivity(activityId: number): Observable<any> {
+        return this.http.delete<any>(`${this.secondLevelUrl}/${activityId}`);
+    }
+
+    // getActivitiesForProgram(programId): Observable<Activity[]> {
+    //   return this.http.get<Activity[]>(`${this.secondLevelUrl}/?workflowlevel1__id=${programId}`);
+    // }
 }
 
