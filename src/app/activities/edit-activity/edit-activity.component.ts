fe1ec3532d6a02083405dea6e60fefc48d4417dd
Youssef Sherif <sharief@aucegypt.edu>
2018-12-01 07:54:02 +0200

    Add edit activity component

--- src/app/activities/edit-activity/edit-activity.component.ts
+++ src/app/activities/edit-activity/edit-activity.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Activity } from '../models/Activity';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { State, selectProgramName, selectActivityName } from 'src/app/reducers';
+import { Observable } from 'rxjs';
+import { EditActivity } from '../activities.actions';
+
 
 @Component({
-  selector: 'app-edit-activity',
-  templateUrl: './edit-activity.component.html',
+    selector: 'app-edit-activity',
+    templateUrl: './edit-activity.component.html',
 })
 export class EditActivityComponent implements OnInit {
 
-  constructor() { }
+    programId: string;
+    activityId: string;
+    programName$: Observable<string>;
+    activityName$: Observable<string>;
 
-  ngOnInit() {
-  }
+    constructor(
+        private store: Store<State>,
+        private route: ActivatedRoute,
+    ) { }
+
+    ngOnInit() {
+        this.programId = this.route.snapshot.params['programId'];
+        this.activityId = this.route.snapshot.params['activityId'];
+        this.programName$ = this.store.select(selectProgramName(this.programId));
+        this.activityName$ = this.store.select(selectActivityName(this.activityId));
+    }
+
+    onSubmitted(x: Activity) {
+        this.store.dispatch(new EditActivity(parseInt(this.activityId, 10), x));
+    }
 
 }
