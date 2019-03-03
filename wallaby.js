b848ef572b9a8be836a7fb912e0fceefc5b308d3
Simon McEnlly <simon.mcenlly@gmail.com>
2019-03-04 09:37:17 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,11 +1,14 @@
 module.exports = function (wallaby) {
-
-  const jestTransform = file => require('jest-preset-angular/preprocessor').process(file.content, file.path, {globals: {__TRANSFORM_HTML__: true}, rootDir: __dirname});
+  const jestTransform = file => require('jest-preset-angular/preprocessor').process(file.content, file.path, {
+    globals: {
+      __TRANSFORM_HTML__: true,
+    },
+    rootDir: __dirname
+  });
 
   return {
     files: [
       'src/**/*.+(ts|html|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
-      'jest.setup.ts',
       '!src/**/*.spec.ts',
     ],
 
@@ -33,6 +36,16 @@ module.exports = function (wallaby) {
       ]
     },
 
-    testFramework: 'jest'
+    setup: wallaby => {
+      const jestConfig = require('jest-preset-angular/jest-preset.json');
+      jestConfig.setupFilesAfterEnv = ['<rootDir>/src/setup-jest.js'];
+      jestConfig.transform = {
+        '^.*node_modules.*\\.(ts|js|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
+      };
+      wallaby.testFramework.configure(jestConfig);
+    },
+
+    testFramework: 'jest',
   };
 };
+ 
